a33cc5d898b0308e2deda7afd53ff155ad6c90a8
Dmitry Chernysh <cheerful.che@gmail.com>
2019-03-11 23:25:30 +1100

    Refactor chess-board

--- chess-board.py
+++ chess-board.py
@@ -1,52 +1,34 @@
-newPage(200, 200)
-w = width()
-h = height()
+w = 200
+h = 200
+newPage(w, h)
 
-dark = .2
 light = .8
+dark = .2
 
 even = light
 odd = dark
 
-mySide = 20
+mySide = 24
 
 rows = h // mySide
-cols = rows
+cols = w // mySide
 paddingV = (h % mySide) / 2
 paddingH = (w % mySide) / 2
 
-print("rows:", rows, "\rpadding:", paddingH, "\r ")
+rowCounter = 0
+while rowCounter < rows:
+    y = paddingV + mySide * rowCounter
+    rowCounter += 1
 
-def isEven(counter):
-    if counter % 2 != 0:
-        return 1
-    else:
-        return 0
-
-rowCounter = rows
-
-while rowCounter > 0:
-    if isEven(rowCounter):
-        fill(odd)
-    else:
-        fill(even)
-    print(rowCounter)
-    rect(paddingH, paddingV + mySide * (rowCounter - 1), mySide, mySide)
-    rowCounter -= 1
-    
-    colCounter = cols
-    while colCounter > 1:
-        if isEven(rowCounter):
-            if isEven(colCounter):
-                fill(even)
-            else:
-                fill(odd)
+    colCounter = 0
+    while colCounter < cols:
+        x = paddingH + mySide * colCounter
+        if (rowCounter + colCounter) % 2 == 0:
+            fill(odd)
         else:
-            if isEven(colCounter):
-                fill(odd)
-            else:
-                fill(even)
+            fill(even)
 
-        print('  ', colCounter)
-        rect(paddingH + mySide * (colCounter - 1), paddingV + mySide * (rowCounter), mySide, mySide)
-        colCounter -= 1
+        rect(x, y, mySide, mySide)
+        colCounter += 1
+
+print(rows, "×", cols, "\rpadding:", paddingH)
